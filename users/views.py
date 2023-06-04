a152f946d2c76a010efee2d1ae4e472dc9a13eaf
Shaif Bin Kalim <sbkalim@ymail.com>
2023-06-04 14:53:21 +0600

    Finish of Flights App

---
+++ users/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+# Create your views here.
+
+
+def index(request):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse('login'))
+
+def login_view(request):
+    return render(request, 'user/login.html')
+
+def logout_view(request):
+    pass
